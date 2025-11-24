977c08fbe8ea1d5358e33c48e3bebaea5df96e68
JUDOKA104 <e.oriol@ecole-ipssi.net>
2025-11-24 11:43:04 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
-// If you publish under https://<user>.github.io/<repo>, set base to '/<repo>' later.
 export default defineConfig({
   integrations: [react()],
   output: 'static',
-  site: 'https://judoka104.github.io/snakes/', // replace with your real URL later
+  site: 'https://judoka104.github.io/snakes',
+  base: '/',
   prefetch: true
 });
